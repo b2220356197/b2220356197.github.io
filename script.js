56268f383b1ee2a38ad007407eb42549c236c1bb
Ezel Bayraktar <115808971+b2220356197@users.noreply.github.com>
2023-09-09 21:04:15 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+// Animate the header on page load
+const header = document.querySelector('header');
+header.style.transform = 'scale(0)';
+header.style.opacity = '0';
 
+window.addEventListener('load', () => {
+    header.style.transition = 'transform 0.5s, opacity 0.5s';
+    header.style.transform = 'scale(1)';
+    header.style.opacity = '1';
+});
+
+// Add more JavaScript interactions as needed
